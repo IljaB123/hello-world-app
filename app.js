9211aa902a80d00f477205a5661096e93c8ca17e
IljaB <118997578+IljaB123@users.noreply.github.com>
2024-11-04 16:31:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+module.exports = app
