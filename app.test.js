205517c5577d4399bacb94f9955bd850fae631ef
IljaB <118997578+IljaB123@users.noreply.github.com>
2024-11-04 16:30:20 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,9 +1,10 @@
-// app.test.js
 const request = require('supertest');
+const express = require('express');
+
 const app = require('./app');
 
-describe('GET /', () => {
-  it('should respond with Hello World', async () => {
+describe('Express.js App', () => {
+  it('responds with Hello World and status 200', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello World');
